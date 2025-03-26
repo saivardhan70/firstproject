e5626a610190487a1dcf8a1cc47a4ef9675e735a
saivardhan70 <saivardhan70@gmail.com>
2025-03-26 16:57:35 +0530

    new line added

--- PageRepository/LoginPage.ts
+++ PageRepository/LoginPage.ts
@@ -22,4 +22,8 @@ export class LoginPage {
     async invoicesTab(){
         await this.page.click(this.locators.invoicesTab);
     }
+
+    async login(){
+        await this.page.click(this.locators.invoicesTab)
+    }
 }
